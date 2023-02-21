95979d662e37343bb36edf8d660186356492fd65
_p4lm4 <matejadjokic23@gmail.com>
2023-02-21 23:38:58 +0100

    new

--- sketch.js
+++ sketch.js
@@ -1,13 +1,46 @@
 
-let a,b;
+let path;
 
 function setup() {
   createCanvas(400, 400);
 
-  a= createVector(100,-60);
-  b= createVector(200,60);
+  path = createVector(200, 60);
+}
+
+function vectirProjection(a, path) {
+  let bC = path.copy().normalize();
+  let sp = a.dot(bC);
+  bC.mult(sp);
+  return bC;
 }
 
 function draw() {
-  background(220);
+  background(0);
+  strokeWeight(4);
+  stroke(255);
+
+  let pos = createVector(100, 200);
+
+  let mouse = createVector(mouseX, mouseY);
+  let a = p5.Vector.sub(mouse, pos);
+
+  //line(pos.x, pos.y, pos.x + a.x, pos.y + a.y);
+  line(pos.x, pos.y, pos.x + path.x, pos.y + path.y);
+
+  let v = vectirProjection(a, path);
+
+  //strokeWeight(8);
+  //stroke(0, 0, 255);
+  //line(pos.x, pos.y, pos.x + v.x, pos.y + v.y); 
+
+  //strokeWeight(1);
+  //line(pos.x + a.x, pos.y + a.y, v.x + pos.x, v.y + pos.y);
+
+  fill(0, 255, 0);
+  noStroke();
+  circle(pos.x+a.x, pos.y+a.y, 16);
+
+  fill(255, 0, 0);
+  noStroke();
+  circle(v.x + pos.x, v.y + pos.y, 16);
 }
